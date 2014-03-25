df5bd4ff21763ca8a8cd749775a642ec521230ca
Mark Jackson <rednineinfo@gmail.com>
2014-03-25 16:47:52 +0000

    modified this to accept MetaHIK for the finger pose controls. Hopefully this will help people see how we can use the poseHandlers. NOTE: HIK joint orients, by default will NOT load between left and right hands :( bloody arse!

--- tests/testFiles/MetaRig_Poses/fingers/poseHandler.py
+++ tests/testFiles/MetaRig_Poses/fingers/poseHandler.py
@@ -38,29 +38,29 @@ of nodes that are then pushed into the poseData for processing.
 
 def getNodesOverload(poseObj,nodes,*args):
 
-	#NOTE: poseObj already has an attr 'metaRig' which is filled  
-	#automatically in the main buildInternalPoseData() call
-	metaNode=poseObj.metaRig
+    #NOTE: poseObj already has an attr 'metaRig' which is filled  
+    #automatically in the main buildInternalPoseData() call
+    metaNode=poseObj.metaRig
 
-	#catch the currently selected node
-	currentSelection=cmds.ls(sl=True,l=True)
+    #catch the currently selected node
+    currentSelection=cmds.ls(sl=True,l=True)
 
-	#see if we have a left or right controller selected and switch to the
-	#appropriate subMetaSystem
-	if cmds.getAttr('%s.mirrorSide' % currentSelection[0])==1:
-		filteredNodes=metaNode.L_ArmSystem.L_Fingers_System.getChildren()
-		[filtered.append(node) for node in cmds.listRelatives(filtered,type='joint',ad=True,f=True)]
-		
-	elif cmds.getAttr('%s.mirrorSide' % currentSelection[0])==2:
-		filteredNodes=metaNode.R_ArmSystem.R_Fingers_System.getChildren()
-		[filtered.append(node) for node in cmds.listRelatives(filtered,type='joint',ad=True,f=True)]
-		
-	#modify the actual PoseData object, changing the data to be matched on index
-	#rather than using the standard name or metaMap matching
-	poseObj.metaPose=False
-	poseObj.matchMethod='index'
-	
-	return filteredNodes
+    #see if we have a left or right controller selected and switch to the
+    #appropriate subMetaSystem
+    if cmds.getAttr('%s.mirrorSide' % currentSelection[0])==1:
+        filteredNodes=metaNode.L_ArmSystem.L_Fingers_System.getChildren()
+        [filtered.append(node) for node in cmds.listRelatives(filtered,type='joint',ad=True,f=True)]
+        
+    elif cmds.getAttr('%s.mirrorSide' % currentSelection[0])==2:
+        filteredNodes=metaNode.R_ArmSystem.R_Fingers_System.getChildren()
+        [filtered.append(node) for node in cmds.listRelatives(filtered,type='joint',ad=True,f=True)]
+        
+    #modify the actual PoseData object, changing the data to be matched on index
+    #rather than using the standard name or metaMap matching
+    poseObj.metaPose=False
+    poseObj.matchMethod='index'
+    
+    return filteredNodes
 
 
 In the most basic case you could just construct a list of nodes
@@ -71,32 +71,36 @@ and return that!
 import Red9.core.Red9_Meta as r9Meta
 import maya.cmds as cmds
 
-	
-def getNodesOverload(poseObj,nodes,*args):
 
-	#NOTE: poseObj already has an attr 'metaRig' which is filled  
-	#automatically in the main buildInternalPoseData() call
-	metaNode=poseObj.metaRig
+def getNodesOverload(poseObj, nodes, *args):
 
-	#catch the currently selected node
-	currentSelection=cmds.ls(sl=True,l=True)
-
-	#see if we have a left or right controller selected and switch to the
-	#appropriate subMetaSystem
-	if cmds.getAttr('%s.mirrorSide' % currentSelection[0])==1:
-		filteredNodes=metaNode.L_ArmSystem.L_Fingers_System.getChildren()
-		[filteredNodes.append(node) for node in cmds.listRelatives(filteredNodes,type='joint',ad=True,f=True)]
-		
-	elif cmds.getAttr('%s.mirrorSide' % currentSelection[0])==2:
-		filteredNodes=metaNode.R_ArmSystem.R_Fingers_System.getChildren()
-		[filteredNodes.append(node) for node in cmds.listRelatives(filteredNodes,type='joint',ad=True,f=True)]
-		
-	#modify the actual PoseData object, changing the data to be matched on index
-	#rather than using the standard name or metaMap matching
-	poseObj.metaPose=False
-	poseObj.matchMethod='index'
-	
-	return filteredNodes
+    # NOTE: poseObj already has an attr 'metaRig' which is filled
+    # automatically in the main buildInternalPoseData() call
+    metaNode = poseObj.metaRig
+    currentSelection = cmds.ls(sl=True, l=True)
+    filteredNodes=[]
+    if not issubclass(type(metaNode), r9Meta.MetaHIKControlSetNode):
+        # see if we have a left or right controller selected and switch to the
+        # appropriate subMetaSystem
+        if cmds.getAttr('%s.mirrorSide' % currentSelection[0]) == 1:
+            filteredNodes = metaNode.L_ArmSystem.L_Fingers_System.getChildren()
+            [filteredNodes.append(node) for node in cmds.listRelatives(filteredNodes, type='joint', ad=True, f=True)]
+            
+        elif cmds.getAttr('%s.mirrorSide' % currentSelection[0]) == 2:
+            filteredNodes = metaNode.R_ArmSystem.R_Fingers_System.getChildren()
+            [filteredNodes.append(node) for node in cmds.listRelatives(filteredNodes, type='joint', ad=True, f=True)]
+    else:
+        if currentSelection[0] == metaNode.LeftWristEffector or currentSelection[0] == metaNode.LeftHand:
+            [filteredNodes.append(node) for node in cmds.listRelatives(metaNode.LeftHand, type='joint', ad=True, f=True)]
+        elif currentSelection[0] == metaNode.RightWristEffector or currentSelection[0] == metaNode.RightHand:
+            [filteredNodes.append(node) for node in cmds.listRelatives(metaNode.RightHand, type='joint', ad=True, f=True)]
+            
+    # modify the actual PoseData object, changing the data to be matched on index
+    # rather than using the standard name or metaMap matching
+    poseObj.metaPose = False
+    poseObj.matchMethod = 'index'
+    
+    return filteredNodes
 
 
 
@@ -105,34 +109,33 @@ def getNodesOverload(poseObj,nodes,*args):
 #=================================================
 
 def poseGetNodesLoad(poseObj,nodes,*args):
-	'''
-	PoseLoad:
-	this is an entry point used to over-load the main getNodes()
-	function in the PoseData object. This allows you to modify, on 
-	the fly the poseObj itself as poseObj arg is the class instance
-	@param poseObj: the actual instance of the PoseData object
-	@param nodes: original node list passed in from the UI 
-	'''
-	return getNodesOverload(poseObj,nodes,*args)
-	
+    '''
+    PoseLoad:
+    this is an entry point used to over-load the main getNodes()
+    function in the PoseData object. This allows you to modify, on 
+    the fly the poseObj itself as poseObj arg is the class instance
+    @param poseObj: the actual instance of the PoseData object
+    @param nodes: original node list passed in from the UI 
+    '''
+    return getNodesOverload(poseObj,nodes,*args)
+    
 def poseGetNodesSave(poseObj,nodes,*args):
-	'''
-	PoseSave:
-	this is an entry point used to over-load the main getNodes()
-	function in the PoseData object. This allows you to modify, on 
-	the fly the poseObj itself as poseObj arg is the class instance
-	@param poseObj: the actual instance of the PoseData object
-	@param nodes: original node list passed in from the UI 
-	'''
-	return getNodesOverload(poseObj,nodes,*args)
-	
-def posePopupAdditions(parent):
-	'''
-	This run when the Pose PopUp menu is generated, allows us to add custom menu's to the 
-	popUp and extend it's functionality as we need at a folder level!
-	'''
-	cmds.menuItem(divider=True)
-	cmds.menuItem(parent=parent,label='Test Finger Menu 1!', command="print('Added Test Menu 1')")
-	cmds.menuItem(parent=parent,label='Test Finger Menu 2!', command="print('Added Test Menu 2')")
-
+    '''
+    PoseSave:
+    this is an entry point used to over-load the main getNodes()
+    function in the PoseData object. This allows you to modify, on 
+    the fly the poseObj itself as poseObj arg is the class instance
+    @param poseObj: the actual instance of the PoseData object
+    @param nodes: original node list passed in from the UI 
+    '''
+    return getNodesOverload(poseObj,nodes,*args)
+    
+def posePopupAdditions(parent, ui=None):
+    '''
+    This run when the Pose PopUp menu is generated, allows us to add custom menu's to the 
+    popUp and extend it's functionality as we need at a folder level!
+    '''
+    cmds.menuItem(divider=True)
+    cmds.menuItem(parent=parent,label='Test Finger Menu 1!', command="print('Added Test Menu 1')")
+    cmds.menuItem(parent=parent,label='Test Finger Menu 2!', command="print('Added Test Menu 2')")
 
